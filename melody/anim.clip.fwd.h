dfdc9bbd3d8178dd26ff2e7e1247652dfffeba62
Gabbo <gabbo@Gabrieles-MacBook-Pro.local>
2026-03-07 18:12:27 +0100

    wip

--- melody/anim.clip.fwd.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "core.types.h"
-
-typedef struct Mel_Anim_Clip_Event Mel_Anim_Clip_Event;
-typedef struct Mel_Anim_Clip Mel_Anim_Clip;
--- melody/anim.track.fwd.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "core.types.h"
-
-typedef struct Mel_Anim_Track Mel_Anim_Track;
-typedef void (*Mel_Blend_Fn)(f32* out, const f32* a, const f32* b, u32 stride, f32 t);
