54bbb6c4cdff593f7a6b71336aac36ca8e89dd44
0xABB0 <gabbodb@gmail.com>
2026-05-31 21:10:13 +0200

    wip

--- modules/display/build.c
+++ modules/display/build.c
@@ -1,7 +1,8 @@
 #include "build.h"
 
-void build(Mel_Build *b) {
-    Mel_Target *lib = mel_add_library(b, "display");
+void build(Mel_Build* b)
+{
+    Mel_Target* lib = mel_add_library(b, "display");
     mel_includes(lib, MEL_PUBLIC, ALWAYS, "include");
     mel_sources(lib, ALWAYS, "src/display.c", "src/events.c");
     mel_sources(lib, WHEN(.platforms = MEL_ON(MACOS)), "src/macos/*.m");
@@ -9,8 +10,9 @@ void build(Mel_Build *b) {
     mel_sources(lib, WHEN(.platforms = MEL_ON(WIN32)), "src/win32/*.c");
     mel_sources(lib, WHEN(.platforms = MEL_ON(ANDROID)), "src/android/*.c");
     mel_sources(lib, WHEN(.platforms = MEL_ON(IOS)), "src/ios/*.m");
-    mel_link(lib, MEL_PUBLIC, WHEN(.platforms = MEL_ON(MACOS)), "-framework", "CoreGraphics",
-             "-framework", "IOKit");
+
+    mel_link(lib, MEL_PUBLIC, WHEN(.platforms = MEL_ON(MACOS)), "-framework", "CoreGraphics", "-framework", "IOKit");
+  
     mel_depends(lib, "core");
     mel_depends(lib, "allocator");
     mel_depends(lib, "collection");
@@ -18,6 +20,5 @@ void build(Mel_Build *b) {
     mel_depends(lib, "platform");
     mel_depends(lib, "reflect");
 
-    mel_codegen(lib, "enum-str-gen", "display.enum.gen.c", "$out", "$cflags", "$hostclang", "--",
-                "display/display.h");
+    mel_codegen(lib, "enum-str-gen", "display.enum.gen.c", "$out", "$cflags", "$hostclang", "--", "display/display.h");
 }
