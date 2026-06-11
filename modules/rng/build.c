36e760d5b26657cb506626788982ed80ac8d7720
0xABB0 <gabbodb@gmail.com>
2026-06-11 16:05:10 +0200

    rng: add wasm entropy stub via crypto.getRandomValues

--- modules/rng/build.c
+++ modules/rng/build.c
@@ -8,5 +8,6 @@ void build(Mel_Build* b)
     mel_sources(lib, WHEN(.platforms = MEL_ON(MACOS) | MEL_ON(IOS) | MEL_ON(LINUX) | MEL_ON(ANDROID)), "posix/src/*.c");
     mel_sources(lib, WHEN(.platforms = MEL_ON(WIN32)), "win32/src/*.c");
     mel_link(lib, MEL_PUBLIC, WHEN(.platforms = MEL_ON(WIN32)), "-lbcrypt");
+    mel_sources(lib, WHEN(.platforms = MEL_ON(WASM)), "wasm/src/*.c");
     mel_depends(lib, "core");
 }
